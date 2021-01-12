eda0a06b38a5a2ad2b91f00357c677b2e2e77a2b
David Millen <70208036+dm-devtech@users.noreply.github.com>
2021-01-12 18:17:57 +0000

    added functionality to remove To-dos

--- spec/TodosSpec.js
+++ spec/TodosSpec.js
@@ -5,12 +5,28 @@ describe("Todos", function() {
     todoList = new Todos();
   });
 
-  it("adding a todo returns confirmation message and the todo", function() {
-    expect(todoList.add("wash the car")).toEqual("Todo Added. Todos remaining: wash the car");
-  });
+  describe("Add to-do function", function() {
+    it("adding a todo returns confirmation message and the todo", function() {
+      expect(todoList.add("wash the car")).toEqual("Todo Added. Todos remaining: wash the car");
+    });
 
-  it("adds multiple todos and returns them", function() {
-    todoList.add("wash the dishes")
-    expect(todoList.add("clean the car")).toEqual("Todo Added. Todos remaining: wash the dishes, clean the car")
+    it("adds multiple todos and returns them", function() {
+      todoList.add("wash the dishes")
+      expect(todoList.add("clean the car")).toEqual("Todo Added. Todos remaining: wash the dishes, clean the car")
+    })
   })
+
+  describe("Remove to-do function", function() {
+    it("delete a to do", function() {
+      todoList.add("wash the dishes")
+      expect(todoList.done(1)).toEqual("You dont have any todos!")
+    })
+
+    it("add multiple todos and delete one of them, returns remaining todo", function() {
+      todoList.add("wash the dishes")
+      todoList.add("clean the car")
+      expect(todoList.done(1)).toEqual("Todos remaining: clean the car")
+    })
+  })
+
 });
--- src/Todos.js
+++ src/Todos.js
@@ -9,4 +9,16 @@ class Todos {
     let allTodos = this.list.join(", ")
     return `Todo Added. Todos remaining: ${allTodos}`
     }
+
+  done(toDoItem) {
+    let element = toDoItem - 1
+    this.list.splice(element, 1)
+    let allTodos = this.list.join(", ")
+      if(this.list.length === 0) {
+        return "You dont have any todos!"
+      } else {
+        return `Todos remaining: ${allTodos}`
+      }
+    }
+
   }
